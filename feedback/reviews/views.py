068bfe2cd98f3c71458ec3f17219ec58bbbad628
manujagobind <manujagobind@gmail.com>
2021-10-26 22:55:39 +0530

    Replace manual form with django form

--- feedback/reviews/views.py
+++ feedback/reviews/views.py
@@ -1,12 +1,18 @@
 from django.shortcuts import render
 from django.http.response import HttpResponseRedirect
 
+from .forms import ReviewForm
+
 # Create your views here.
 
 def review(request):
     if request.method == 'POST':
         return HttpResponseRedirect("/thank-you")
-    return render(request, 'reviews/review.html')
+
+    form = ReviewForm()
+    return render(request, 'reviews/review.html', {
+        "form": form
+    })
 
 def thank_you(request):
     return render(request, 'reviews/thank-you.html')

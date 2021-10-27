9f625833205207a42184a1be6a1329e640988d8d
manujagobind <manujagobind@gmail.com>
2021-10-27 21:38:51 +0530

    Using model forms

--- feedback/reviews/forms.py
+++ feedback/reviews/forms.py
@@ -1,9 +1,29 @@
 from django import forms
 
-class ReviewForm(forms.Form):
-    user_name = forms.CharField(label="Your Name", max_length=100, error_messages={
-        "max_length": "Please enter a shorter name",
-        "required": "Your name must not be empty"
-    })
-    review_text = forms.CharField(label="Your feedback", widget=forms.Textarea, max_length=200)
-    rating = forms.IntegerField(label="Your rating", min_value=1, max_value=5)
+from .models import Review
+
+# class ReviewForm(forms.Form):
+#     user_name = forms.CharField(label="Your Name", max_length=100, error_messages={
+#         "max_length": "Please enter a shorter name",
+#         "required": "Your name must not be empty"
+#     })
+#     review_text = forms.CharField(label="Your feedback", widget=forms.Textarea, max_length=200)
+#     rating = forms.IntegerField(label="Your rating", min_value=1, max_value=5)
+
+
+class ReviewForm(forms.ModelForm):
+
+    class Meta:
+        model = Review
+        fields = '__all__'
+        labels = {
+            "user_name": "Your name",
+            "review_text": "Your feedback",
+            "rating": "Your rating"
+        }
+        error_messages = {
+            "user_name": {
+                "required": "Your name must not be empty",
+                "max_length": "Please enter a shorter name"
+            }
+        }
--- feedback/reviews/views.py
+++ feedback/reviews/views.py
@@ -2,7 +2,6 @@ from django.shortcuts import render
 from django.http.response import HttpResponseRedirect
 
 from .forms import ReviewForm
-from .models import Review
 
 # Create your views here.
 
@@ -10,12 +9,7 @@ def review(request):
     if request.method == 'POST':
         form = ReviewForm(request.POST)
         if form.is_valid():
-            review = Review(
-                user_name=form.cleaned_data['user_name'],
-                review_text=form.cleaned_data['review_text'],
-                rating=form.cleaned_data['rating']
-            )
-            review.save()
+            form.save()
             return HttpResponseRedirect("/thank-you")
     else:
         form = ReviewForm()

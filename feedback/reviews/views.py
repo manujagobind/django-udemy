a044ddb529c169aee30dcb1b40c3363e71fdaff0
manujagobind <manujagobind@gmail.com>
2021-10-27 21:33:44 +0530

    Store from data to db

--- feedback/reviews/views.py
+++ feedback/reviews/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.http.response import HttpResponseRedirect
 
 from .forms import ReviewForm
+from .models import Review
 
 # Create your views here.
 
@@ -9,6 +10,12 @@ def review(request):
     if request.method == 'POST':
         form = ReviewForm(request.POST)
         if form.is_valid():
+            review = Review(
+                user_name=form.cleaned_data['user_name'],
+                review_text=form.cleaned_data['review_text'],
+                rating=form.cleaned_data['rating']
+            )
+            review.save()
             return HttpResponseRedirect("/thank-you")
     else:
         form = ReviewForm()

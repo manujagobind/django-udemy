cf80a6d5f09762d864166d7f1e7738f7dd950a31
manujagobind <manujagobind@gmail.com>
2021-10-27 21:22:11 +0530

    Add more fields and customize form

--- feedback/reviews/forms.py
+++ feedback/reviews/forms.py
@@ -1,4 +1,9 @@
 from django import forms
 
 class ReviewForm(forms.Form):
-    user_name = forms.CharField()
+    user_name = forms.CharField(label="Your Name", max_length=100, error_messages={
+        "max_length": "Please enter a shorter name",
+        "required": "Your name must not be empty"
+    })
+    review_text = forms.CharField(label="Your feedback", widget=forms.Textarea, max_length=200)
+    rating = forms.IntegerField(label="Your rating", min_value=1, max_value=5)
